e24166e5395a3f93b1ce95680492cb0095a9cfaf
Adetunji Ojekunle <ojekunleadetunji@gmail.com>
2023-11-07 07:07:19 +0100

    update routes with product handlers

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
+import {
+  createProduct,
+  deleteProduct,
+  updateProduct,
+} from "./handlers/product";
 
 const router = Router();
 
-router.get("/product", () => {});
-router.post("/product", () => {});
-router.put("/product", () => {});
+// router.get("/product", () => {});
+router.post("/product", createProduct);
+router.put("/product/:id", updateProduct);
+router.delete("/product/:id", deleteProduct);
 
 export default router;
